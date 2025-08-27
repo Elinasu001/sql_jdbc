68dc964302651f1c8af1ee868b53029555a5499d
Elinasu001 <elianen001@gmail.com>
2025-08-27 17:50:26 +0900

    TRUNCATE

--- Script-1.sql
+++ Script-1.sql
@@ -136,38 +136,125 @@ SELECT
 -- 처리결과 :: Updated Rows : 3개 행 INSERT -> 정수값 3 돌려줌
 --------------------------------------------------------------------------------
 /*
+	2. UPDATE
+	테이블에 기록된 기존의 데이터를 수정하는 구문
+    
+    [ 표현법 ]
+    UPDATE 
+           테이블명
+       SET
+           컬럼명 = 바꿀값
+         , 컬럼명 = 바꿀값
+         , ...  => 여러 개의 컬럼값을 바꿀 수 있음(, 써야함!! AND 아님 !!!! ★★★★)
+     WHERE
+           조건식; => 생략 가능 단, 조건이 있으면 조건식 써줘야됨
+*/
+----테이블 복사하기
+CREATE TABLE DEPT_COPY2 -- 복사 ~
+    AS SELECT * FROM DEPARTMENT;
 
+SELECT * FROM DEPT_COPY; -- 땡겨 ~
 
+-- DEPT_COPY 테이블에서 총무부를 부서명을 미래사업부로 변경
+UPDATE 
+       DEPT_COPY
+   SET
+       DEPT_TITLE = '미래사업부';
+--출력 결과 Updated Rows : 9 ...? 
 
+SELECT * FROM DEPT_COPY; -
+--전체 행의 모든 DEPT_TITLE 컬럼 값이 전부 미래사업부로 UPDATE 되버림 우리는 총무부를 미래사업부로 변경하고 싶으니 다시 ROLLBACK;
+ROLLBACK; -- 아... auto commit...DEPT_COPY2로
+
+UPDATE 
+       DEPT_COPY2
+   SET
+       DEPT_TITLE = '미래사업부'
+ WHERE
+	   DEPT_TITLE = '총무부';
+SELECT * FROM DEPT_COPY2;
+-- 이제 총무부만 미래사업부로 변경됨.
+
+-- 그럼 어떤 상황이어야지 WHERE절이 안붙나?
+-- 김하늘 100원 보너스
+UPDATE
+       EMPLOYEE
+   SET
+       SALARY = 100
+ WHERE
+       --EMP_NAME = '김하늘'; --> 사실 좋은 건 아님 WHY? 중복이 있을 수 있기 때문이다. 그럼 겹치지 않는 갑으로 하는게 좋음
+	   EMP_ID = 204;
+
+-- 전체 사원들 급여를 기존 급여에서 5% 인상해
+UPDATE 
+       EMPLOYEE
+   SET 
+       SALARY = SALARY * 1.05;
+SELECT * FROM EMPLOYEE;
+
+-------------------------------------------------------------------------
+/*
+	UPDATE 사용 시 서브쿼리 사용해보기
+	
+	UPDATE
+	       테이블명
+	   SET
+	       컬럼명 = (서브쿼리)
+	 WHERE
+	       조건; -- 생략 가능
+	       
+	※주의 :: 조건 안 사용하면 전부 삭제됨
 */
 
+-- 안예성 사원의 급여를 김하늘 사원의 급여만큼으로 갱신
+-- 중복 고려하기
+UPDATE 
+       EMPLOYEE
+   SET
+       SALARY = (SELECT --> = 대입의 용도
+                        SALARY
+                   FROM
+                        EMPLOYEE
+                  WHERE
+                        EMP_NAME = '김하늘') -- 그럼 이건 ? 중복이 발생하지 않는가?
+ WHERE
+       EMP_ID = (SELECT --> = 동등비교의 용도 : 중복 발생 예방
+                        EMP_ID
+                   FROM
+                        EMPLOYEE
+                  WHERE
+                        EMP_NAME = '안예성');
 
+SELECT * FROM EMPLOYEE;
 
+--------------------------------------------------------------------------
+/*
+ 	3. DELETE
+ 	테이블에 기록된 행을 삭제하는 구문
+ 	
+ 	[ 표현법 ]
+ 	DELETE
+ 	  FROM 
+ 	       테이블명
+ 	 WHERE
+ 	       조건; => 생략가능
+ 	※주의 :: UPDATE와 동일하게 WHERE 조건 절을 작성하지 않으면 모든 행 삭제 될 수 있음.※
+ */
 
+SELECT * FROM DEPT_COPY2;
+DELETE
+  FROM
+       DEPT_COPY2
+ WHERE
+       DEPT_TITLE = '미래사업부';
+SELECT * FROM DEPT_COPY2;
 
+-------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+	TRUNCATE : 테이블의 전체 행을 삭제하는 사용하는 구문(절삭)
+	 		   DELETE 보다 빠름 (이론적으로............)
+	 		   별도의 조건을 부여할 수 없음 ROOLBACK 불가능 !!!!
+*/
+DELETE FROM DEPT_COPY; -- 0.0004s
+TRUNCATE TABLE DEPT_COPY2; -- 0.063s  이론적으로 물리적 위치와 지우려는 데이터의 양에 따라 달라질 수 있음... 수십 만개일 경우 차이 날 수 있음
